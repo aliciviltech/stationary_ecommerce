20db0bb48c716df532869ec1216f96e7dedc34cb
aliciviltech <ali.civiltech@gmail.com>
2025-01-01 15:11:06 +0500

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
-  return (
-    <div className='Header w-[95%] max-w-[1400px] mx-auto h-16  flex items-center justify-between px-10'>
-        <div className="logo text-[20px]">
-            <span className=' text-[var(--primaryColor)]'>Stationary</span>Store
+    const [showMenu, setShowMenu] = useState(false);
+    return (
+        <div className={`Header w-[95%] max-w-[1400px] mx-auto h-16  flex items-center justify-between px-10
+            ${!showMenu ? 'flex' : 'hidden'}
+        `}>
+            <div className="logo text-[20px]">
+                <span className=' text-[var(--primaryColor)]'>Stationary</span>Store
+            </div>
+            {/* <div className="search">
+                <input className='px-2 bg-slate-100 rounded-lg' type="text" placeholder='Search' />
+            </div>
+            <div className="navBar flex gap-6">
+                <a className='hover:text-[var(--primaryColor)]' href="">Home</a>
+                <a className='hover:text-[var(--primaryColor)]' href="">About</a>
+                <a className='hover:text-[var(--primaryColor)]' href="">Categories</a>
+                <a className='hover:text-[var(--primaryColor)]' href="">Contact Us</a>
+            </div> */}
+
+            <div className="menuBars">
+                <i className='fa-solid fa-bars'></i>
+            </div>
         </div>
-        <div className="search">
-            <input className='px-2 bg-slate-100 rounded-lg' type="text" placeholder='Search' />
-        </div>
-        <div className="navBar flex gap-6">
-            <a className='hover:text-[var(--primaryColor)]' href="">Home</a>
-            <a className='hover:text-[var(--primaryColor)]' href="">About</a>
-            <a className='hover:text-[var(--primaryColor)]' href="">Categories</a>
-            <a className='hover:text-[var(--primaryColor)]' href="">Contact Us</a>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
